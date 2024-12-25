9a6fcd9f5b9dd022fd4a5680a3fbdf82cd2d6682
neural <neural01@proton.me>
2024-12-24 23:47:16 -0500

    Updated Framework Base

--- Framework.lua
+++ Framework.lua
@@ -1,10 +1,6 @@
---[[ Framework.lua
-  Main base of the repository.
-]]
-
 local filename = "Framework"
 
-local Import = ({...})[1][1] or nil
+local Import = ({...})[1] or nil
 if not Import then error("Import not provided to "..filename) end
 
 Import("Test")
