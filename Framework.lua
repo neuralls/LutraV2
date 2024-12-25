b3b44f2fdf6c27496a90ab78eea108d44e1ba107
neural <neural01@proton.me>
2024-12-24 23:59:35 -0500

    import check edit

--- Framework.lua
+++ Framework.lua
@@ -1,6 +1,10 @@
+--[[ Framework.lua
+  Main base of the repository.
+]]
+
 local filename = "Framework"
 
 local Import = ({...})[1] or nil
-if not Import then error("Import not provided to "..filename) end
-
-Import("Test")
+if typeof(Import) ~= "function" then
+  error("Import not provided to "..filename)
+end
